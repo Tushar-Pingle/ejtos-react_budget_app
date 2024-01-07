8818eedcde28a1c1912f2134291f7342b4eda4c1
Tushar Pingle <tush.pingle@gmail.com>
2024-01-07 05:16:44 -0500

    changes made to the currency.js file and the code is executing now

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,30 +1,36 @@
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Currency = () => {
-    const [setCurrency] = useContext(AppContext);
+  const { dispatch } = useContext(AppContext);
 
-    const handleCurrencyChange = (event) => {
-        const currencySymbolMap ={
-            Dollar: '$',
-            Pound: '£',
-            Euro: '€',
-            Rupee: '₹'
-        };
-        setCurrency(currencySymbolMap[event.traget.value])
-    }
+  const changeLocation = (val) => {
+    dispatch({
+      type: "CHG_CURRENCY",
+      payload: val,
+    });
+  };
 
-    return (
-        <div className="currency-container">
-            <label htmlFor="currency-select" className="currency-label">Currency ({Currency})</label>
-            <select id="currency-select" className="form-select" value={Currency} onChange={handleCurrencyChange}>
-                <option value="Dollar">$ Dollar</option>
-                <option value="Pound">£ Pound</option>
-                <option value="Euro">€ Euro</option>
-                <option value="Rupee">₹ Rupee</option>
-            </select>
-        </div>
-    );
+  return (
+    <div className="alert alert-secondary d-flex  flex-md-row">
+      Currency  
+      {
+        <select
+          className="form-select form-select-sm mx-lg-2  w-75"
+          name="Currency"
+          id="Currency"
+          onChange={(event) => changeLocation(event.target.value)}
+        >
+        
+
+          <option value="$">$ Dollar</option>
+          <option value="£">£ Pound</option>
+          <option value="€">€ Euro</option>
+          <option value="₹">₹ Ruppee</option>
+        </select>
+      }
+    </div>
+  );
 };
 
 export default Currency;
